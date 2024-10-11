0b82a73190f4826cbe43e190411acbb42f35bd85
usman-cmd <usman62111@gmail.com>
2024-10-12 00:01:38 +0500

    add deploy workflow

--- src/main.jsx
+++ src/main.jsx
@@ -6,10 +6,10 @@ import App from "./App.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <BrowserRouter>
+    <HashRouter>
       <ChakraProvider>
         <App />
       </ChakraProvider>
-    </BrowserRouter>
+    </HashRouter>
   </StrictMode>
 );
